4e56c3117912ab884c933dff53129be62410114e
Depal1 <47154119+Depal1@users.noreply.github.com>
2022-07-10 00:13:48 -0600

    further grammar corrections

--- PlayCover/PlayCoverError.swift
+++ PlayCover/PlayCoverError.swift
@@ -18,15 +18,15 @@ extension PlayCoverError: LocalizedError {
         case .infoPlistNotFound:
             return NSLocalizedString("This .IPA is courrupted. It doesn't contains Info.plist.", comment: "")
         case .waitInstallation:
-            return NSLocalizedString("Please, wait current install to finish!", comment: "")
+            return NSLocalizedString("Please, wait for the current install to finish!", comment: "")
         case .playcoverReinstall:
-            return NSLocalizedString("Please, reinstall PlayCover app", comment: "")
+            return NSLocalizedString("Please, reinstall the PlayCover app", comment: "")
         case .appEncrypted:
             return NSLocalizedString("App is encrypted! Please, use decrypted .ipa from AppDb or download one from the internal store. iMazing .ipa are not currently supported!", comment: "")
         case .appCorrupted:
             return NSLocalizedString("Something went wrong with this .ipa. Please try to use another .ipa file.", comment: "")
         case .appProhibited:
-            return NSLocalizedString("You'll receive ban on this PlayCover version! Call of Duty supported on 0.9.2-0.9.4. PUBG requires higher PlayCover version. Free Fire status is not confirmed. There is one message of ban.", comment: "")
+            return NSLocalizedString("You'll receive a ban on this PlayCover version! Call of Duty is supported on 0.9.2-0.9.4. PUBG requires a higher PlayCover version. Free Fire's status is not confirmed. There is one message of ban.", comment: "")
     }
     }
 }
