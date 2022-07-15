3173a169857ac7796e7383ac1bfd8e8b0e805cfe
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2022-07-15 13:51:14 +0800

    Moved verify migrate logic to Migrate class

--- PlayCover/View/PlayCoverApp.swift
+++ PlayCover/View/PlayCoverApp.swift
@@ -31,11 +31,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 			defaults: ["NSApplicationCrashOnExceptions" : true]
 		)
         LaunchServicesWrapper.setMyselfAsDefaultApplicationForFileExtension("ipa")
-        if !fm.fileExists(atPath: "/Users/\(NSUserName())/Library/Containers/io.playcover.PlayCover/migrate") {
-                    Migrate.migrateData()
-        }
+        Migrate.verifyMigrateState()
     }
-    
 }
 
 @main
