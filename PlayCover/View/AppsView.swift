3ed10a65bd40fd00f1c89f42df12c58422be6384
Adam C <66894537+Wind-Explorer@users.noreply.github.com>
2022-07-18 12:29:40 +0800

    Repositioned search bar position
    Shifted it to align with other buttons originally above it

--- PlayCover/View/AppsView.swift
+++ PlayCover/View/AppsView.swift
@@ -38,6 +38,7 @@ struct AppsView : View {
                 }.padding(.leading, 30)
                     .help("Untick this option to show installed apps only")
                 Spacer()
+                SearchView().padding(.leading, 20).padding(.trailing, 25).padding(.vertical, 8)
                 ExportView().environmentObject(InstallVM.shared)
                 Button(NSLocalizedString("Download more apps", comment: "")) {
                     if let url = URL(string: "https://armconverter.com/decryptedappstore") {
@@ -46,12 +47,6 @@ struct AppsView : View {
                 }.buttonStyle(OutlineButton()).controlSize(.large).help(NSLocalizedString("Use this site to decrypt and download any global app", comment:""))
                     .padding(.trailing, 30)
             }
-
-            HStack(alignment: .center){
-                Spacer()
-                SearchView().padding(.leading, 20).padding(.trailing, 25).padding(.vertical, 8)
-//                Image("Happy").resizable().frame(width: 64, height: 64).padding(.bottom, 0).padding(.trailing, 16) // "remove the damm cat" - Perseque
-            }.padding(.top, 0)
             Divider().padding(.top, 0).padding(.horizontal, 36)
 			if !sh.isXcodeCliToolsInstalled {
 				VStack(spacing: 12) {
