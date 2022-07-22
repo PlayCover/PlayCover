0194648c8c277ff11b53b2ef67f0aa32fe40e5de
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2022-07-22 01:56:16 -0400

    Localisation stuff

--- PlayCover/Services/SoundDeviceService.swift
+++ PlayCover/Services/SoundDeviceService.swift
@@ -24,16 +24,15 @@ class SoundDeviceService {
         
         DispatchQueue.main.async {
             let alert = NSAlert()
-            alert.messageText = "Incorrect Audio Settings Detected!"
-            alert.informativeText = "Your current output device does not have a sample rate of 48 or 44.1 KHz!" +
-                "Crashes may occur. Would you like to change your current output device's sample rate to 48 KHz?"
-            alert.addButton(withTitle: "OK")
-            alert.addButton(withTitle: "Cancel")
+            alert.messageText = NSLocalizedString("soundAlert.messageText", comment: "")
+            alert.informativeText = NSLocalizedString("soundAlert.informativeText", comment: "")
+            alert.addButton(withTitle: NSLocalizedString("OK", comment: ""))
+            alert.addButton(withTitle: NSLocalizedString("Cancel", comment: ""))
             alert.alertStyle = .critical
             let response: NSApplication.ModalResponse = alert.runModal()
             if response == NSApplication.ModalResponse.alertFirstButtonReturn {
                 device?.setNominalSampleRate(48000.0)
-                Log.shared.msg("Current output device sample rate set to 48 KHz")
+                Log.shared.msg(NSLocalizedString("soundAlert.successText", comment: ""))
             }
         }
         
