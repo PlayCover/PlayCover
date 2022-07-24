539ebd41db832c70d206d04589b24bbab9e66122
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2022-07-24 17:27:19 -0400

    Remove deprecated and unused Objective-C services (#81)
    * Remove Deprecated and Potentially Parasitic Objective-C
    
    * Fixed Deprecation
    
    * Fixed animation deprecation warning
    
    * Revert "Fixed animation deprecation warning"
    
    This reverts commit 68e3c0957312a4e772be1593e7b21c4c22b4fff0.
    
    Co-authored-by: Isaac Marovitz <isaacmarovitz@gmail.com>

--- PlayCover/Utils/LoginKitWrapper.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-NS_ASSUME_NONNULL_BEGIN
-
-@interface LoginKitWrapper : NSObject
-+(BOOL) setLogin:(LSSharedFileListRef) inlist path:(NSString *) path;
-@end
-
-NS_ASSUME_NONNULL_END
