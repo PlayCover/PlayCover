f014eb5955bcd246e7a6b40bbd9b94a3288b5c5e
Isaac Marovitz <42140194+IsaacMarovitz@users.noreply.github.com>
2022-08-05 00:40:13 +0100

    Properly selected view, fixed navbar style

--- PlayCover/View/MainView.swift
+++ PlayCover/View/MainView.swift
@@ -13,15 +13,19 @@ struct MainView: View {
 
     @Binding public var xcodeCliInstalled: Bool
 
+    @State private var selectedView: Int? = 0
+
     var body: some View {
         NavigationView {
             List {
-                NavigationLink(destination: {
-                    AppLibraryView()
-                }, label: {
+                NavigationLink(destination: AppLibraryView(), tag: 1, selection: self.$selectedView) {
                     Label("App Library", systemImage: "square.grid.2x2")
-                })
+                }
             }
+            .listStyle(.sidebar)
+        }
+        .onAppear {
+            self.selectedView = 1
         }
     }
 }
@@ -31,5 +35,8 @@ struct MainView_Previews: PreviewProvider {
 
     static var previews: some View {
         MainView(xcodeCliInstalled: $xcodeCliInstalled)
+            .environmentObject(InstallVM.shared)
+            .environmentObject(AppsVM.shared)
+            .environmentObject(AppIntegrity())
     }
 }
