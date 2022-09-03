e0571353b9901c78f876eb26c29cd00b7c6aae0c
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2022-09-03 14:50:44 +0700

    fix!: detect encrypted Mach-O only on the 64-bit slice

--- PlayCover/Utils/PlayTools.swift
+++ PlayCover/Utils/PlayTools.swift
@@ -43,8 +43,12 @@ class PlayTools {
     }
 
     static func isMachoEncrypted(atURL url: URL) throws -> Bool {
-        let otoolOutput = try shell.shello(otool.path, "-l", url.path)
-        if otoolOutput.contains("LC_ENCRYPTION_INFO") && otoolOutput.contains("cryptid 1") {return true}
+        // Split output into blocks
+        let otoolOutput = try shell.shello(otool.path, "-l", url.path).components(separatedBy: "Load command")
+        // Check specifically for encryption info on the 64 bit block
+        for block in otoolOutput where (block.contains("LC_ENCRYPTION_INFO_64") && block.contains("cryptid 1")) {
+            return true
+        }
         return false
     }
 
