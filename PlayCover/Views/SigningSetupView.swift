e8a79c1cb8e8dfb06ebea68fe13a88042be8a330
ohaiibuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2022-09-08 22:33:50 +0700

    fix!: Use the current system status to determine if PlaySign should be enabled

---
+++ PlayCover/Views/SigningSetupView.swift
@@ -0,0 +1,20 @@
+//
+//  SignSetupView.swift
+//  PlayCover
+//
+//  Created by Venti on 08/09/2022.
+//
+
+import SwiftUI
+
+struct SignSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignSetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignSetupView()
+    }
+}
