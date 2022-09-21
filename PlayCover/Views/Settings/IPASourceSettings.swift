921142388d17b5bf997c3637b3bc63808454990f
Isaac Marovitz <isaacryu@icloud.com>
2022-09-21 16:05:11 -0400

    Link validator

--- PlayCover/Views/Settings/IPASourceSettings.swift
+++ PlayCover/Views/Settings/IPASourceSettings.swift
@@ -107,19 +107,42 @@ struct IPASourceSettings: View {
     }
 }
 
+enum SourceValidation {
+    case valid, badurl, badjson, checking
+}
+
 struct AddSourceView: View {
     @State var newSource = ""
+    @State var newSourceURL: URL?
+    @State var sourceValidationState = SourceValidation.checking
     @Binding var sources: [SourceData]
     @Binding var addSourceSheet: Bool
 
     var body: some View {
         VStack {
             TextField(text: $newSource, label: {Text("Source URL...")})
+            Spacer()
+                .frame(height: 20)
             HStack {
-                Image(systemName: "checkmark.circle.fill")
-                    .foregroundColor(.green)
-                Text("Link Valid")
-                    .font(.system(.subheadline))
+                switch sourceValidationState {
+                case .valid:
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(.green)
+                    Text("Link valid")
+                        .font(.system(.subheadline))
+                case .badurl:
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.red)
+                    Text("URL invalid")
+                        .font(.system(.subheadline))
+                case .badjson:
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundColor(.red)
+                    Text("JSON not found or invalid")
+                        .font(.system(.subheadline))
+                case .checking:
+                    EmptyView()
+                }
                 Spacer()
                 Button(action: {
                     addSourceSheet.toggle()
@@ -127,17 +150,56 @@ struct AddSourceView: View {
                     Text("Cancel")
                 })
                 Button(action: {
-                    sources.append(SourceData(source: newSource))
-                    addSourceSheet.toggle()
+                    if newSourceURL != nil {
+                        sources.append(SourceData(source: newSourceURL!.absoluteString))
+                        addSourceSheet.toggle()
+                    }
                 }, label: {
                     Text("Done")
                 })
                 .tint(.accentColor)
                 .keyboardShortcut(.defaultAction)
+                .disabled(sourceValidationState != .valid)
             }
         }
         .padding()
         .frame(width: 400, height: 100)
+        .onChange(of: newSource) { source in
+            validateSource(source)
+        }
+    }
+
+    func validateSource(_ source: String) {
+        sourceValidationState = .checking
+        DispatchQueue.global(qos: .userInteractive).async {
+            if let url = URL(string: source) {
+                newSourceURL = url
+                if StoreVM.checkAvaliability(url: newSourceURL!) {
+                    do {
+                        if newSourceURL!.scheme == nil {
+                            newSourceURL = URL(string: "https://" + newSourceURL!.absoluteString)!
+                        }
+                        let contents = try String(contentsOf: newSourceURL!)
+                        let jsonData = contents.data(using: .utf8)!
+                        do {
+                            let data: [StoreAppData] = try JSONDecoder().decode([StoreAppData].self, from: jsonData)
+                            if data.count > 0 {
+                                sourceValidationState = .valid
+                                return
+                            }
+                        } catch {
+                            sourceValidationState = .badjson
+                            return
+                        }
+                    } catch {
+                        sourceValidationState = .badurl
+                        return
+                    }
+                }
+            }
+            sourceValidationState = .badurl
+            return
+        }
     }
 }
 
