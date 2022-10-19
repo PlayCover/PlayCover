2bd4366a02fe6f1bc2a44518f8926b9e632a9b90
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2022-10-19 10:46:22 -0600

    feat: app uninstall settings (#383)
    * feat: app uninstall settings
    
    * fix: localisation and lint
    
    * add uninstall options to uninstall popup
    
    * simplify uninstall message
    
    * add suppression tooltip + simplify code
    
    * clear all app cache in prune + simplify code
    
    * simplify uninstall settings
    
    Use KVC for settings variables
    
    * simplify redundant texts

--- PlayCover/Views/Settings/PlayCoverSettingsView.swift
+++ PlayCover/Views/Settings/PlayCoverSettingsView.swift
@@ -13,7 +13,7 @@ struct PlayCoverSettingsView: View {
     @EnvironmentObject var storeVM: StoreVM
 
     private enum Tabs: Hashable {
-        case updates, ipasource
+        case updates, ipasource, uninstall
     }
 
     var body: some View {
@@ -23,6 +23,11 @@ struct PlayCoverSettingsView: View {
                     Label("preferences.tab.updates", systemImage: "square.and.arrow.down")
                 }
                 .tag(Tabs.updates)
+            UninstallSettings.shared
+                .tabItem {
+                  Label("preferences.tab.uninstall", systemImage: "trash.square")
+                }
+                .tag(Tabs.uninstall)
             IPASourceSettings()
                 .tabItem {
                     Label("preferences.tab.ipasource", systemImage: "list.bullet")
---
+++ PlayCover/Views/Settings/UninstallSettings.swift
@@ -0,0 +1,58 @@
+//
+//  UninstallSettings.swift
+//  PlayCover
+//
+//  Created by TheMoonThatRises on 9/26/22.
+//
+
+import SwiftUI
+
+class UninstallPreferences: NSObject, ObservableObject {
+    static var shared = UninstallPreferences()
+
+    @objc @AppStorage("ClearAppDataUninstall") var clearAppData = false
+    @objc @AppStorage("RemoveAppKeymapUninstall") var removeAppKeymap = false
+    @objc @AppStorage("RemoveAppSettingUninstall") var removeAppSettings = false
+    @objc @AppStorage("RemoveAppEntitlementsUninstall") var removeAppEntitlements = false
+
+    @AppStorage("ShowUninstallPopup") var showUninstallPopup = true
+}
+
+struct UninstallSettings: View {
+    public static var shared = UninstallSettings()
+
+    @ObservedObject var uninstallPreferences = UninstallPreferences.shared
+
+    @State private var showPruneFileAlert = false
+
+    var body: some View {
+        Form {
+            HStack {
+                Text("preferences.whenUninstalling")
+                VStack(alignment: .leading) {
+                    Toggle("preferences.toggle.showUninstall", isOn: $uninstallPreferences.showUninstallPopup)
+                    Toggle("preferences.toggle.clearAppData", isOn: $uninstallPreferences.clearAppData)
+                    Toggle("preferences.toggle.removeKeymap", isOn: $uninstallPreferences.removeAppKeymap)
+                    Toggle("preferences.toggle.removeSetting", isOn: $uninstallPreferences.removeAppSettings)
+                    Toggle("preferences.toggle.removeEntitlements", isOn: $uninstallPreferences.removeAppEntitlements)
+                }
+            }
+            Button("preferences.button.pruneFiles") {
+                showPruneFileAlert.toggle()
+            }
+            .alert("preferences.prune.alert", isPresented: $showPruneFileAlert, actions: {
+                Button("preferences.button.pruneFiles", role: .destructive) {
+                    Uninstaller.pruneFiles()
+                }
+                Button("button.Cancel", role: .cancel) {
+                    showPruneFileAlert.toggle()
+                }
+                .keyboardShortcut(.defaultAction)
+            }, message: {
+                Text("preferences.prune.message")
+            })
+        }
+        .padding(30)
+        .frame(width: 400, height: 170, alignment: .center)
+    }
+}
