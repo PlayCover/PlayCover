9b40b2a54fe5aa69fa48e34fc846c8553b83ceac
Zhich <nick.zhicheng@gmail.com>
2022-11-16 02:10:48 -0500

    Localisation conformance

--- PlayCover/Utils/AlertExtensions.swift
+++ PlayCover/Utils/AlertExtensions.swift
@@ -10,12 +10,12 @@ import AppKit
 extension NSAlert {
     static func differentBundleIdKeymap(response: (NSApplication.ModalResponse) -> Void) {
         let alert = NSAlert()
-        alert.messageText = "The name of the keymap is different from the bundle ID of the app!"
-        alert.informativeText = "The keymap may have been created for another application."
+        alert.messageText = NSLocalizedString("alert.differentBundleIdKeymap.message", comment: "")
+        alert.informativeText = NSLocalizedString("alert.differentBundleIdKeymap.text", comment: "")
         alert.alertStyle = .warning
         alert.addButton(withTitle: NSLocalizedString("button.Proceed", comment: ""))
         alert.addButton(withTitle: NSLocalizedString("button.Cancel", comment: ""))
-        
+
         let result = alert.runModal()
         response(result)
     }
