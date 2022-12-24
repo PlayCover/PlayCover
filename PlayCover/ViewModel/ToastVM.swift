94e13c77ca377612a6788ddcd89785098d79a7f3
Amir Mohammadi <amirkamangar@icloud.com>
2022-12-24 22:38:18 +0330

    Optional ToastView Show + Display Name of the App/Game in Download State (#621)
    * Optional Show + Name of App being Downloading
    
    * fix used locale key
    
    * Update Localizable.strings
    
    * Update ToastView.swift

--- PlayCover/ViewModel/ToastVM.swift
+++ PlayCover/ViewModel/ToastVM.swift
@@ -11,6 +11,7 @@ class ToastVM: ObservableObject {
     static let shared = ToastVM()
 
     @Published var toasts: [ToastInfo] = []
+    @Published var isShown: Bool = true
 
     func showToast(toastType: ToastType, toastDetails: String) {
         toasts.append(ToastInfo(toastType: toastType, toastDetails: toastDetails, timeRemaining: 2))
