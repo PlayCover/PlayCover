d3e557c3d6f1758057ba1dda2b3b9c9e44132be3
Isaac Marovitz <isaacryu@icloud.com>
2023-01-17 14:26:00 -0500

    Ignore file_length

--- PlayCover/Utils/PlayTools.swift
+++ PlayCover/Utils/PlayTools.swift
@@ -7,6 +7,7 @@ import Foundation
 import injection
 
 // swiftlint:disable type_body_length
+// swiftlint:disable file_length
 
 class PlayTools {
     private static let frameworksURL = FileManager.default.homeDirectoryForCurrentUser
