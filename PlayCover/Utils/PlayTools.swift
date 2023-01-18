9d0f0c81b7efe6d192a980c985cf05847e291495
Isaac Marovitz <isaacryu@icloud.com>
2023-01-17 23:19:22 -0500

    3rd times the charm

--- PlayCover/Utils/PlayTools.swift
+++ PlayCover/Utils/PlayTools.swift
@@ -339,8 +339,8 @@ class PlayTools {
         var versionCommand = build_version_command(cmd: UInt32(LC_BUILD_VERSION),
                                                    cmdsize: 24,
                                                    platform: UInt32(PLATFORM_MACCATALYST),
-                                                   minos: 11 << 2,
-                                                   sdk: 14 << 2,
+                                                   minos: 0x00000b00,
+                                                   sdk: 0x00000e00,
                                                    ntools: 0)
 
         var zero: UInt = 0
