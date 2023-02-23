89185357e14cd610668da59aee17aec464781b8b
OHaiiBuzzle <23693150+ohaiibuzzle@users.noreply.github.com>
2023-02-23 12:37:37 +0700

    feat: (slightly) more robust uri handler (#538)
    * feat: (slightly) more robust URI Handler
    
    * fix: behavior tweaks
    
    * chore: undo Cartfile.resolved changes
    
    * fix: use host instead of path for uri parsing
    
    * chore: remove print statement
    
    * fix: even tighter checks

--- PlayCover/Views/MainView.swift
+++ PlayCover/Views/MainView.swift
@@ -29,6 +29,9 @@ struct MainView: View {
     @State private var xcodeInstallStatus: XcodeInstallStatus = .installing
     @State private var selectedBackgroundColor: Color = Color.accentColor
     @State private var selectedTextColor: Color = Color.black
+
+    @ObservedObject private var URLObserved = URLObservable.shared
+
     var body: some View {
         GeometryReader { viewGeom in
             NavigationView {
@@ -88,7 +91,7 @@ struct MainView: View {
                 .background(SplitViewAccessor(sideCollapsed: $collapsed))
             }
             .onAppear {
-                self.selectedView = 1
+                self.selectedView = URLObserved.type == .source ? 2 : 1
             }
             .toolbar {
                 ToolbarItem(placement: .navigation) {
@@ -216,6 +219,9 @@ struct MainView: View {
                 .sheet(isPresented: $isSigningSetupShown) {
                     SignSetupView(isSigningSetupShown: $isSigningSetupShown)
                 }
+            .onChange(of: URLObserved.action) { _ in
+                self.selectedView = URLObserved.type == .source ? 2 : self.selectedView
+            }
         }
         .frame(minWidth: 675, minHeight: 330)
     }
--- PlayCover/Views/PlayCoverApp.swift
+++ PlayCover/Views/PlayCoverApp.swift
@@ -8,18 +8,7 @@ import SwiftUI
 class AppDelegate: NSObject, NSApplicationDelegate {
     func application(_ application: NSApplication, open urls: [URL]) {
         if let url = urls.first {
-            if url.pathExtension == "ipa" {
-                uif.ipaUrl = url
-                Installer.install(ipaUrl: uif.ipaUrl!, export: false, returnCompletion: { _ in
-                    Task { @MainActor in
-                        AppsVM.shared.apps = []
-                        AppsVM.shared.fetchApps()
-                        NotifyService.shared.notify(
-                            NSLocalizedString("notification.appInstalled", comment: ""),
-                            NSLocalizedString("notification.appInstalled.message", comment: ""))
-                    }
-                })
-            }
+            URLHandler.shared.processURL(url: url)
         }
     }
 
