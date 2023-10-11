1e8e74c6ed9146e349dd9af0a1685208f7d44b68
TheMoonThatRises <58153205+TheMoonThatRises@users.noreply.github.com>
2023-10-10 18:56:15 -0600

    fix error when container does not exist

--- PlayCover/Utils/Extensions/PlayAppExtensions.swift
+++ PlayCover/Utils/Extensions/PlayAppExtensions.swift
@@ -9,37 +9,41 @@ import Foundation
 
 extension PlayApp {
     func loadDiscordIPC() {
-        let appTmp = self.container.containerUrl.appendingPathComponent("Data")
-            .appendingPathComponent("tmp")
+        if self.container.doesExist() {
+            let appTmp = self.container.containerUrl.appendingPathComponent("Data")
+                .appendingPathComponent("tmp")
 
-        appTmp.enumerateContents(options: []) { url, type in
-            if url.lastPathComponent.contains("discord-ipc-") && (type.isSymbolicLink ?? true) {
-                FileManager.default.delete(at: url)
-            }
-        }
+            try? FileManager.default.createDirectory(at: appTmp, withIntermediateDirectories: false)
 
-        guard self.settings.settings.discordActivity.enable else {
-            return
-        }
-
-        let userTmp = FileManager.default.temporaryDirectory.path
-
-        for ipcPort in 0..<10 {
-            let socketPath = userTmp + "/discord-ipc-\(ipcPort)"
-            if FileManager.default.fileExists(atPath: socketPath) {
-                do {
-                    try FileManager.default.createSymbolicLink(atPath: appTmp
-                        .appendingPathComponent("discord-ipc-\(ipcPort)").path,
-                                                               withDestinationPath: socketPath)
-                    print("Successfully linked discordipc for \(self.info.bundleIdentifier)")
-                    return
-                } catch {
-                    print(error)
-                    continue
+            appTmp.enumerateContents(options: []) { url, type in
+                if url.lastPathComponent.contains("discord-ipc-") && (type.isSymbolicLink ?? true) {
+                    FileManager.default.delete(at: url)
                 }
             }
-        }
 
-        print("Unable to link discordipc for \(self.info.bundleIdentifier)")
+            guard self.settings.settings.discordActivity.enable else {
+                return
+            }
+
+            let userTmp = FileManager.default.temporaryDirectory.path
+
+            for ipcPort in 0..<10 {
+                let socketPath = userTmp + "/discord-ipc-\(ipcPort)"
+                if FileManager.default.fileExists(atPath: socketPath) {
+                    do {
+                        try FileManager.default.createSymbolicLink(atPath: appTmp
+                            .appendingPathComponent("discord-ipc-\(ipcPort)").path,
+                                                                   withDestinationPath: socketPath)
+                        print("Successfully linked discordipc for \(self.info.bundleIdentifier)")
+                        return
+                    } catch {
+                        print(error)
+                        continue
+                    }
+                }
+            }
+
+            print("Unable to link discordipc for \(self.info.bundleIdentifier)")
+        }
     }
 }
