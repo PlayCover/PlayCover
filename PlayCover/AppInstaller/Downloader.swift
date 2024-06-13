ff6f45f5d6b68ae5982cf69cd1afc116f0b0f346
Catta1997 <edoardo.cattarin.97@gmail.com>
2024-06-13 22:42:11 +0200

    updated alert style
    Added "destructive" style to download anyway so it's clearer that is a not recommended action

--- PlayCover/AppInstaller/Downloader.swift
+++ PlayCover/AppInstaller/Downloader.swift
@@ -48,7 +48,7 @@ class DownloadApp {
                 )
                 alert.alertStyle = .warning
                 alert.addButton(withTitle: NSLocalizedString("Ok", comment: ""))
-                alert.addButton(withTitle: NSLocalizedString("alert.download.downloadAnyway", comment: ""))
+                alert.addButton(withTitle: NSLocalizedString("alert.download.downloadAnyway", comment: "")).hasDestructiveAction = true
                 if alert.runModal() == .alertFirstButtonReturn {
                     return
                 }
