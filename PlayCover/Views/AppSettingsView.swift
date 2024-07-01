e16a5d8ec478810765d89b8008d8a1af8b96686a
Depal1 <47154119+Depal1@users.noreply.github.com>
2024-07-01 17:49:13 -0600

    Change RAM iPad M4

--- PlayCover/Views/AppSettingsView.swift
+++ PlayCover/Views/AppSettingsView.swift
@@ -213,7 +213,7 @@ struct GraphicsView: View {
                         Text("iPad Pro (12.9-inch) (3rd gen) | A12X | 4GB").tag("iPad8,6")
                         Text("iPad Pro (12.9-inch) (5th gen) | M1 | 8GB").tag("iPad13,8")
                         Text("iPad Pro (12.9-inch) (6th gen) | M2 | 8GB").tag("iPad14,5")
-                        Text("iPad Pro (13-inch) (7th gen) | M4 | 16GB").tag("iPad16,6")
+                        Text("iPad Pro (13-inch) (7th gen) | M4 | 8GB").tag("iPad16,6")
                         Divider()
                         Text("iPhone 13 Pro Max | A15 | 6GB").tag("iPhone14,3")
                         Text("iPhone 14 Pro Max | A16 | 6GB").tag("iPhone15,3")
