946044c2ac159011ddf8b1ed18ce7adfc9d4a81f
Catta1997 <edoardo.cattarin.97@gmail.com>
2025-09-18 13:18:31 +0200

    icons

--- PlayCover/Views/MenuBarView.swift
+++ PlayCover/Views/MenuBarView.swift
@@ -11,11 +11,11 @@ struct PlayCoverMenuView: Commands {
     @Binding var isSigningSetupShown: Bool
     var body: some Commands {
         CommandGroup(after: .systemServices) {
-            Button("menubar.log.copy") {
+            Button("menubar.log.copy", systemImage: "document.on.document.fill") {
                 Log.shared.logdata.copyToClipBoard()
             }
             .keyboardShortcut("L", modifiers: [.command, .option])
-            Button("menubar.configSigning") {
+            Button("menubar.configSigning", systemImage: "signature") {
                 isSigningSetupShown = true
             }
         }
@@ -31,13 +31,13 @@ struct PlayCoverHelpMenuView: Commands {
         }
 
         CommandGroup(replacing: .help) {
-            Button("menubar.documentation") {
+            Button("menubar.documentation", systemImage: "document.fill") {
                 if let url = URL(string: "https://docs.playcover.io") {
                     NSWorkspace.shared.open(url)
                 }
             }
             Divider()
-            Button("menubar.website") {
+            Button("menubar.website", systemImage: "network") {
                 if let url = URL(string: "https://playcover.io") {
                     NSWorkspace.shared.open(url)
                 }
@@ -54,7 +54,7 @@ struct PlayCoverHelpMenuView: Commands {
             }
             #if DEBUG
             Divider()
-            Button("[DEBUG] Crash app") {
+            Button("[DEBUG] Crash app", systemImage: "xmark.circle.fill") {
                 fatalError("Crash was triggered")
             }
             #endif
@@ -66,7 +66,7 @@ struct PlayCoverViewMenuView: Commands {
     var body: some Commands {
         CommandGroup(replacing: .newItem) {}
         CommandGroup(replacing: .importExport) {
-            Button("menubar.exportToSideloady") {
+            Button("menubar.exportToSideloady", systemImage: "square.and.arrow.up.fill") {
                 Task {
                     if InstallVM.shared.inProgress {
                         Log.shared.error(PlayCoverError.waitInstallation)
@@ -107,7 +107,7 @@ struct PlayCoverViewMenuView: Commands {
             }
         }
         CommandGroup(before: .sidebar) {
-            Button("menubar.clearCache") {
+            Button("menubar.clearCache", systemImage: "eraser.fill") {
                 DataCache.instance.cleanAll()
                 Cacher.shared.removeImageCache()
 
