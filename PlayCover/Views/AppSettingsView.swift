60d8c2e6163067d3e1d4429b47e80124b7671be9
时雨 <270598250@qq.com>
2026-01-23 00:17:15 +0800

    Update: add M5 iPad Pro devices

--- PlayCover/Views/AppSettingsView.swift
+++ PlayCover/Views/AppSettingsView.swift
@@ -224,6 +224,8 @@ struct GraphicsView: View {
                         Text("iPad Pro (12.9-inch) (5th gen) | M1 | 8GB").tag("iPad13,8")
                         Text("iPad Pro (12.9-inch) (6th gen) | M2 | 8GB").tag("iPad14,5")
                         Text("iPad Pro (13-inch) (7th gen) | M4 | 8GB").tag("iPad16,6")
+                        Text("iPad Pro (11-inch) | M5 | 16GB").tag("iPad17,1")
+                        Text("iPad Pro (13-inch) | M5 | 16GB").tag("iPad17,3")
                         Divider()
                         Text("iPhone 13 Pro Max | A15 | 6GB").tag("iPhone14,3")
                         Text("iPhone 14 Pro Max | A16 | 6GB").tag("iPhone15,3")
