5168ff8c341cf9fa2a100942686c1e0cbcbdf68f
Sinon <34333654+Catta1997@users.noreply.github.com>
2026-03-16 09:04:40 +0100

    feat: settings UI

--- PlayCover/Views/AppSettingsView.swift
+++ PlayCover/Views/AppSettingsView.swift
@@ -142,6 +142,7 @@ struct AppSettingsView: View {
             hasAlias = viewModel.app.hasAlias()
         }
         .padding()
+        .frame(width: 500, height: 500)
     }
 }
 
@@ -711,10 +712,19 @@ struct MiscView: View {
                         Spacer()
                         HStack {
                             Text("settings.text.debugger")
-                            VStack {
-                                Toggle("settings.toggle.lldb", isOn: $settings.openWithLLDB)
-                                Toggle("settings.toggle.lldbWithTerminal", isOn: $settings.openLLDBWithTerminal)
-                                    .disabled(!settings.openWithLLDB)
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Toggle("", isOn: $settings.openWithLLDB)
+                                        .labelsHidden()
+                                    Text("settings.toggle.lldb")
+                                }
+
+                                HStack {
+                                    Toggle("", isOn: $settings.openLLDBWithTerminal)
+                                        .labelsHidden()
+                                        .disabled(!settings.openWithLLDB)
+                                    Text("settings.toggle.lldbWithTerminal")
+                                }
                             }
                         }
                     }
